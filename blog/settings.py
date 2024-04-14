bd7e9abca04cd91e8a4679a376869cd23829abea
AvFl4028 <aviel4028@gmail.com>
2024-04-14 13:50:25 -0600

    update avr q pasa

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2yasy7ha$6i9f7b1*+7-@=1ci=6%^k#bxmhn^#@z5u7i()s(7!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     ".vercel.app",
